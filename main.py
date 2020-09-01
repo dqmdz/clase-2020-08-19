b6bdc5505a0a2dc8ec33cf7c6c8560f815b5c422
dqmdz <daniel.quinterospinto@gmail.com>
2020-09-01 16:39:42 -0300

    update

--- main.py
+++ main.py
@@ -1,46 +1,6 @@
-class Persona():
-    def __init__(self, nombre=''):
-        self.nombre = nombre
-
-    @property
-    def nombre(self):
-        return self.__nombre
-
-    @nombre.setter
-    def nombre(self, value):
-        self.__nombre = value
-
-    def funcionalidad(self):
-        return 'Funcionalidad de persona'
-
-
-class Alumno(Persona):
-    def __init__(self, nombre='', legajo=0):
-        super().__init__(nombre)
-        self.legajo = legajo
-
-    @property
-    def legajo(self):
-        return self.__legajo
-
-    @legajo.setter
-    def legajo(self, value):
-        self.__legajo = value
-
-    def funcionalidad(self):
-        return super().funcionalidad() + ' Necesito que haga otra cosa'
+def suma(a, b):
+    return a + b
 
 
 if __name__ == '__main__':
-    persona = Persona('Sandro')
-    print("Diccionario de persona", persona.__dict__)
-
-    otra_persona = Persona("Michael")
-    print("Diccionario de otra_persona", otra_persona.__dict__)
-
-    alumno = Alumno('Washington', 2000)
-    print("Diccionario de alumno", alumno.__dict__)
-
-    print(persona.funcionalidad())
-    print(otra_persona.funcionalidad())
-    print(alumno.funcionalidad())
+    print(suma(2, 3))
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 import unittest
-from main import Persona
 
 
 class Testing(unittest.TestCase):
-    def test_valida_atributo(self):
-        persona = Persona('Sandro')
-        self.assertEqual(persona.__dict__, {'_Persona__nombre': 'Sandro'})
+    pass
 
 
 if __name__ == '__main__':
