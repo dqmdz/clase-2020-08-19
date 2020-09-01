1d3d571569d25b37d567ebb14e21c97663cb1ccf
dqmdz <daniel.quinterospinto@gmail.com>
2020-09-01 17:38:03 -0300

    update

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import unittest
 from main import suma
+from parameterized import parameterized
 
 
 class Testing(unittest.TestCase):
-    def test_suma(self):
-        result = suma(2, 3)
-        self.assertEqual(result, 5)
+    @parameterized.expand([[2, 3, 5], [3, 4, 7]])
+    def test_suma(self, a, b, c):
+        result = suma(a, b)
+        self.assertEqual(result, c)
 
 
 if __name__ == '__main__':
